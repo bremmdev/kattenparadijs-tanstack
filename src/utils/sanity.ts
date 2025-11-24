b299f8446291c2233ba2860a26911484e0dd9bd7
bremmdev <bremmdev@gmail.com>
2025-11-24 20:53:29 +0100

    use secrets on env

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@sanity/client";
+import { env } from "cloudflare:workers";
 
 export const config = {
   dataset: "production",
-  projectId: "e991dsae",
+  projectId: env.SANITY_PROJECT_ID,
   apiVersion: "2021-08-11", // or today's date for latest
   /**
    * Set useCdn to `false` if your application require the freshest possible

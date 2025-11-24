36432bf5ba15c92f38ba3f8d209c7dcc7a0a8a39
bremmdev <bremmdev@gmail.com>
2025-11-24 10:35:11 +0100

    rm forwardref on modal

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 
-type Props = {
-  children: React.ReactElement;
+type Props = React.ComponentPropsWithRef<"div"> & {
   onClose: (e: React.MouseEvent) => void;
 };
 
-const Modal = React.forwardRef(
-  (props: Props, ref: React.Ref<HTMLDivElement>) => {
-    return (
-      <div
-        id="modal_overlay"
-        ref={ref}
-        className="fixed z-50 inset-0 bg-black/80 w-screen h-screen flex justify-center items-center shrink"
-        onClick={props.onClose}
-      >
-        <div className="absolute inset-1/8 flex items-center justify-center">
-          {props.children}
-        </div>
+const Modal = (props: Props) => {
+  return (
+    <div
+      id="modal_overlay"
+      ref={props.ref}
+      className="fixed z-50 inset-0 bg-black/80 w-screen h-screen flex justify-center items-center shrink"
+      onClick={props.onClose}
+    >
+      <div className="absolute inset-1/8 flex items-center justify-center">
+        {props.children}
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
 export default Modal;
